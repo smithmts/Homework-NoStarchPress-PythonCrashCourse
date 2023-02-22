554042f3dccb425d31d7f29b5a845a6ad75afd1a
smithmts <smithmts+github@gmail.com>
2023-02-21 19:21:20 -0800

    Worked on Chapter 7

---
+++ counting.py
@@ -0,0 +1,10 @@
+# Chapter 7
+
+# while loops.
+
+current_number = 1
+
+# Starts with one, prints the number and adds one until it reaches 5.
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
