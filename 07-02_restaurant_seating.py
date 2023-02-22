68c48258ae8c916515585299dff868c8b693ed95
smithmts <smithmts+github@gmail.com>
2023-02-21 18:55:23 -0800

    Completed Activity 7-2

---
+++ 07-02_restaurant_seating.py
@@ -0,0 +1,13 @@
+# As the user how many people are in their dinner group.
+# If the answer is more than eight, print a message that they will have
+# to wait for a table.  Otherwise, print a message that their table is
+# ready.
+
+people_count = input("Please enter the number of people in your group: ")
+
+people_count = int(people_count)
+
+if people_count >= 8:
+    print("There will be a 20 minute wait.")
+else:
+    print("We have a table ready for you.")
