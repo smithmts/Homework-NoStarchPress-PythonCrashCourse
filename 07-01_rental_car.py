c1674f3614ac023d8473e78638b137d5d74525e7
smithmts <smithmts+github@gmail.com>
2023-02-21 18:50:56 -0800

    Completed Activity 7-1

---
+++ 07-01_rental_car.py
@@ -0,0 +1,7 @@
+# Ask the user what kind of rental car they would like.
+# Print a message about that car, such as "Let me see if I can find you
+# a Subaru"
+
+car_preference = input("Enter the type of car you would like: ")
+
+print(f"Let me see if I can find a {car_preference}.")
