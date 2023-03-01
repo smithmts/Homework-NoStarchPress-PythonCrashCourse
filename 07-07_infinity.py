192a129ba8b16ee34bc8fd1c8407aa0930e23d76
smithmts <smithmts+github@gmail.com>
2023-02-28 18:51:50 -0800

    Completed Activity 7-7

---
+++ 07-07_infinity.py
@@ -0,0 +1,10 @@
+# Writer a loop that never ends.
+# Press CTRL+C or close window displaying the output to run it.
+
+infinatevar = 1
+
+while infinatevar >= 0:
+    print(infinatevar)
+    infinatevar += 1
+
+# I REPEAT.  Press CTRL+C to end.
