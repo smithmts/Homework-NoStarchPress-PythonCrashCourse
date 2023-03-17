fa5a2933dc07f73ca63d2898c356dd2ddad325f6
smithmts <smithmts+github@gmail.com>
2023-03-16 20:44:06 -0700

    Started working on Chapter 8.

---
+++ greeter_ch8.py
@@ -0,0 +1,23 @@
+# Chapter 8
+
+# Simple greeting function.
+
+def greet_user():
+    """Display a simple greeting."""
+    # Functions are documented with a docstring.
+    print("Hello!")
+
+greet_user()
+
+print("...")
+
+# Function with single input.
+
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+
+greet_user('jesse')
+# username in this example is a parameter.
+# 'jesse' is the argument that was past to the function and assigned
+# to the parameter.
