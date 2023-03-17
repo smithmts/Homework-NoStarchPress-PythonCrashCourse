3b1c3e570c7838d78e99c61611986efe8ee4348e
smithmts <smithmts+github@gmail.com>
2023-03-16 20:44:28 -0700

    Completed Activity 8-1.

---
+++ 08-01_message.py
@@ -0,0 +1,8 @@
+# Write a function called `display_message()` that prints one
+# sentance telling everyone what you are learning about in this
+# chapter, then call the function.
+
+def display_message():
+    print("In Chapter 8, I am learning about functions.")
+
+display_message()
