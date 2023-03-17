4f4d27364b198955224ae12292a4a579be61cbfa
smithmts <smithmts+github@gmail.com>
2023-03-16 20:48:13 -0700

    Completed Activity 8-2.

---
+++ 08-02_favorite_book.py
@@ -0,0 +1,10 @@
+# Writer a function called `favorite_book()` that accepts one
+# parameter, `title`.
+# The function should print a message, such as "One of my favorite
+# books is Alice in Wonderland.
+# Call the function, including the book title as an argument.
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title.title()}.")
+
+favorite_book('the drawing of the three')
