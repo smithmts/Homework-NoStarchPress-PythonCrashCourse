bbe663eee4f729efe285967fa134b5a2d8097116
smithmts <smithmts+github@gmail.com>
2023-03-31 09:30:23 -0700

    Completed Activity 8-7.

---
+++ 08-07_album.py
@@ -0,0 +1,26 @@
+# Write a function called make_album() that builds a dictionary
+# describing a music album.
+# Should take an artist name and album title, and return a dictionary
+# With those two pieces of information.
+# Use optional parameter for number of songs on the album.
+# Call at least three three times, one with number of songs.
+
+def make_album(album_name, artist, num_of_songs=None):
+    """Build a dictionary with album information"""
+    album = {
+            'album_name' : album_name,
+            'artist' : artist,
+            }
+    if num_of_songs:
+        album['num_of_songs'] = num_of_songs
+    return album
+
+album = make_album('stone cold rhymin', 'young MC')
+print(album)
+
+album = make_album('living things', 'linkin park')
+print(album)
+
+album = make_album('typhoons', 'royal blood', 11)
+print(album)
+
