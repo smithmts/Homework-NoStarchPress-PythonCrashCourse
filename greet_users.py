4d83e2656efdba7ccb3312b5b43790c40a222a88
smithmts <smithmts+github@gmail.com>
2023-03-31 21:14:33 -0700

    Worked on Chapter 8.

---
+++ greet_users.py
@@ -0,0 +1,12 @@
+# Chapter 8
+
+# Passing a list to a function.
+
+def greet_users(names):
+    """Print a simple greeting to each user in the list."""
+    for name in names:
+        msg = f"Hello, {name.title()}!"
+        print(msg)
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
---
+++ printing_models.py
@@ -0,0 +1,29 @@
+# Chapter 8
+
+# Modifying a list in a function.
+
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design until none are left.
+    Move each design to completed_modesl after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print(f"\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+# Could have combined both steps into one function.
+# However, each function should have one job for better extensibility.
+# You can always use a function to call two other functions.
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
