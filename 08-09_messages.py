c4fc37839439805b4a8438d165a71bc03b8a612a
smithmts <smithmts+github@gmail.com>
2023-04-01 18:30:12 -0700

    Completed Activity 8-9.

---
+++ 08-09_messages.py
@@ -0,0 +1,17 @@
+# Make a list containing a series of short text messages.
+# Pass the list to a function called show_messages(), which prints
+# each message.
+
+messages = [
+        'Arguments can be passed posiitionally or using keywords.',
+        'Defaulted parameters should be listed after any non-defaulted ones.',
+        'Each function should have only one job.',
+        'Pass a copy of a list to a function to prevent modification.'
+        ]
+
+def print_messages(messages):
+    """Print all messages in a list"""
+    for message in messages:
+        print(message)
+
+print_messages(messages)
